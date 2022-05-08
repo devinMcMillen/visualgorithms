6b382432b8cfe516050c2846830c0395c9cfeb40
Devin McMillen <dmcmille90@protonmail.com>
2022-05-08 15:29:24 -0700

    Create base array page. Create base set page

--- visualgorithms/src/app/app.module.ts
+++ visualgorithms/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArrayComponent } from './pages/array/array/array.component';
+import { SetComponent } from './pages/set/set.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArrayComponent,
+    SetComponent
   ],
   imports: [
     BrowserModule,
